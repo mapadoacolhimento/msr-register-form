321781eed405d538f9aed56dced31c7e2ff171bd
Viviane Dias <vivi@mapa.org.br>
2024-06-27 16:17:16 -0300

    feat: adds steps logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,60 @@
 import { Grid } from "@radix-ui/themes";
 import { Header, MainForm, DesktopIllustration, Steps } from "./components";
+import { useState } from "react";
+
+const Fields = () => <div>Fields!</div>;
+const isValid = () => true;
+
+const STEPS = [
+  {
+    title: "Seus dados",
+    Fields: <Fields />,
+    isValid,
+  },
+  {
+    title: "Sobre o acolhimento",
+    subtitle: "Você aceitaria ser atendida online?",
+    Fields: <Fields />,
+    isValid,
+  },
+  {
+    title: "Sobre o acolhimento",
+    subtitle: "Que tipo de acolhimento você precisa?",
+    Fields: <Fields />,
+    isValid,
+  },
+];
 
 function App() {
-  const currentStep = 2;
-  const totalSteps = 3;
-  const progress = (100 * currentStep) / totalSteps;
+  const [totalSteps] = useState(STEPS.length - 1);
+  const [currentStepIndex, setStepIndex] = useState(0);
+  const progress = (100 * currentStepIndex) / totalSteps;
+
+  const goBack = () => {
+    if (currentStepIndex >= 1)
+      return setStepIndex((prevStepIndex) => prevStepIndex - 1);
+    return setStepIndex(0);
+  };
+
+  const goNextStep = () => {
+    const isStepValid = STEPS[currentStepIndex].isValid();
+    if (!isStepValid) return null;
+    setStepIndex((prevStepIndex) => prevStepIndex + 1);
+  };
+
   return (
     <main>
       <Grid columns={{ initial: "1", md: "1fr 55%" }} width="auto">
         <div>
           <Header />
-          <MainForm />
+          <MainForm goBack={goBack} {...STEPS[currentStepIndex]} />
         </div>
         <DesktopIllustration />
       </Grid>
       <Steps
         stepName="Seus dados"
         stepNumber={1}
-        onClick={() => true}
+        onClick={goNextStep}
         isValid
         progress={progress}
       />
--- src/components/MainForm.tsx
+++ src/components/MainForm.tsx
@@ -1,3 +1,27 @@
-export default function MainForm() {
-  return <div>MainForm</div>;
+import { Box, Heading, IconButton, Text } from "@radix-ui/themes";
+import { ChevronLeftIcon } from "@radix-ui/react-icons";
+
+type MainFormProps = {
+  goBack: () => void;
+  title: string;
+  subtitle?: string;
+  Fields: React.ReactNode;
+};
+
+export default function MainForm({
+  goBack,
+  title,
+  subtitle,
+  Fields,
+}: MainFormProps) {
+  return (
+    <Box>
+      <IconButton onClick={goBack}>
+        <ChevronLeftIcon width="24" height="24" />
+      </IconButton>
+      <Heading as={"h1"}>{title}</Heading>
+      {subtitle ? <Text>{subtitle}</Text> : null}
+      {Fields}
+    </Box>
+  );
 }
