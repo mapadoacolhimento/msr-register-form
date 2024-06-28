0f319019d76a4aaf9b42b6afa841970948557a6e
Viviane Dias <vivi@mapa.org.br>
2024-06-28 20:01:32 -0300

    test: adds tests to AcceptsOnlineSupport

---
+++ src/components/MultiStepForm/Steps/__tests__/AcceptsOnlineSupport.spec.tsx
@@ -0,0 +1,54 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import AcceptsOnlineSupport from "../AcceptsOnlineSupport";
+import MultiStepFormWrapper from "../../MultiStepFormWrapper";
+import { Values } from "../..";
+import { sleep } from "../../../../utils";
+
+const setup = () => {
+  return (
+    <MultiStepFormWrapper
+      onSubmit={async (values) =>
+        await sleep(300).then(() => console.log(values))
+      }
+      initialValues={
+        {
+          acceptsOnlineSupport: "",
+        } as Values
+      }
+    >
+      {AcceptsOnlineSupport()}
+    </MultiStepFormWrapper>
+  );
+};
+
+describe("<AcceptsOnlineSupport />", () => {
+  it("should render two options", () => {
+    render(setup());
+
+    expect(
+      screen.getByRole("radio", {
+        name: /sim, aceito ser atendida online/i,
+      }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("radio", {
+        name: /não, só posso receber atendimento presencial/i,
+      }),
+    ).toBeInTheDocument();
+  });
+
+  it("should render an error if no option is selected", async () => {
+    render(setup());
+
+    const btn = screen.getByRole("button", { name: /enviar/i });
+    await userEvent.click(btn);
+
+    await screen.findByRole("alert");
+
+    expect(screen.getByRole("alert")).toHaveTextContent(
+      "Esse campo é obrigatório.",
+    );
+  });
+});
