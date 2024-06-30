900a0394648573de50bf2e773eee4811e5c77725
Viviane Dias <vivi@mapa.org.br>
2024-06-30 19:03:09 -0300

    feat: formats whole repository with new prettier configs

--- cypress/e2e/App.cy.ts
+++ cypress/e2e/App.cy.ts
@@ -1,6 +1,6 @@
 describe("App", () => {
-  it("should render heading", () => {
-    cy.visit("/");
-    cy.findByRole("heading", { name: /vite/i }).should("exist");
-  });
+	it("should render heading", () => {
+		cy.visit("/");
+		cy.findByRole("heading", { name: /vite/i }).should("exist");
+	});
 });
--- src/__tests__/App.spec.tsx
+++ src/__tests__/App.spec.tsx
@@ -3,9 +3,9 @@ import { render, screen } from "@testing-library/react";
 import App from "../App";
 
 describe("App", () => {
-  test("renders headline", () => {
-    render(<App />);
+	test("renders headline", () => {
+		render(<App />);
 
-    expect(screen.getByRole("heading", { name: /vite/i })).toBeInTheDocument();
-  });
+		expect(screen.getByRole("heading", { name: /vite/i })).toBeInTheDocument();
+	});
 });
