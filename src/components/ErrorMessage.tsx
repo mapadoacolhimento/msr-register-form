5027af30e244f3481e5b6a248d5607e02dc3ffef
Viviane Dias <vivi@mapa.org.br>
2024-06-30 18:11:42 -0300

    feat: adds readonly to CustomErrorMessage prop types

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -2,7 +2,7 @@ import { type PropsWithChildren } from "react";
 import { ErrorMessage as FormikErrorMessage } from "formik";
 import { Text } from "@radix-ui/themes";
 
-function CustomErrorMessage({ children }: PropsWithChildren) {
+function CustomErrorMessage({ children }: Readonly<PropsWithChildren>) {
   return (
     <Text role={"alert"} color={"red"} size={"2"} as={"p"}>
       {children}
