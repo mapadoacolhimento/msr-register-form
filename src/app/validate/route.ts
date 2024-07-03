199afcb1dce5df8f4dec860ec4cf749de37b6bc9
Viviane Dias <vivi@mapa.org.br>
2024-07-03 11:29:35 -0300

    feat: adds integration with prisma

--- src/app/validate/route.ts
+++ src/app/validate/route.ts
@@ -1,7 +1,44 @@
+import { db, getErrorMessage } from "../../lib";
+import * as Yup from "yup";
+
 export const runtime = "edge";
 
-export async function POST(request: Request) {
-	const res = await request.json();
+const payloadSchema = Yup.object({
+	email: Yup.string().email().required(),
+	phone: Yup.string().required(),
+});
 
-	return Response.json({ res: res.email });
+export async function POST(request: Request) {
+	try {
+		const payload = await request.json();
+
+		await payloadSchema.validate(payload);
+
+		const msrExistsOnDb = await db.mSRPiiSec.findUnique({
+			where: {
+				email: payload.email,
+				phone: payload.phone,
+			},
+		});
+
+		return Response.json({ isSupportRequestValid: !!msrExistsOnDb });
+	} catch (e) {
+		const error = e as Record<string, unknown>;
+
+		if (error["name"] === "ValidationError") {
+			const errorMsg = `Validation error: ${getErrorMessage(error)}`;
+
+			console.error(`[compose] - [400]: ${errorMsg}`);
+
+			return new Response(errorMsg, {
+				status: 500,
+			});
+		}
+
+		const errorMsg = getErrorMessage(error);
+
+		return new Response(errorMsg, {
+			status: 500,
+		});
+	}
 }
