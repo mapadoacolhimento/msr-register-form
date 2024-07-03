bd5a872511c7038c82d30012227528d09216e530
gloria <gloriateodoro1@gmail.com>
2024-07-03 10:48:49 -0300

    feat: add name, confirmEmail and phone fields

--- src/components/MultiStepForm/Steps/BasicRegisterInformation.tsx
+++ src/components/MultiStepForm/Steps/BasicRegisterInformation.tsx
@@ -8,8 +8,18 @@ import { useState } from "react";
 import TextInput from "@/components/TextInput/TextInput";
 
 const basicRegisterInformationSchema = Yup.object({
+	name: Yup.string().required("Esse campo é obrigatório."),
 	email: Yup.string()
 		.email("Insira um e-mail válido.")
+		.required("Esse campo é obrigatório."),
+	confirmEmail: Yup.string()
+		.oneOf([Yup.ref("email")], "Os e-mails precisam ser iguais.")
+		.required("Esse campo é obrigatório."),
+	phone: Yup.string()
+		.matches(
+			/^\(\d{2}\)\s\d{4,5}-\d{4}$/,
+			"Insira um número de telefone válido com DDD."
+		)
 		.required("Esse campo é obrigatório."),
 });
 
@@ -25,10 +35,29 @@ export default function BasicRegisterInformation() {
 			}}
 		>
 			<TextInput
+				name="name"
+				type="name"
+				label="Nome"
+				placeholder="Qual o seu primeiro nome?"
+			/>
+			<TextInput
 				name="email"
 				type="email"
 				label="E-mail"
 				placeholder="Qual o seu melhor e-mail?"
+			/>
+			<TextInput
+				name="confirmEmail"
+				type="email"
+				label="Confirme seu E-mail"
+				placeholder="Confirme seu e-mail"
+			/>
+			<TextInput
+				name="phone"
+				type="phone"
+				label="Whatsapp"
+				placeholder="Qual o seu whatsapp (com DDD)?"
+				mask="(99) 99999-9999"
 			/>
 		</Step>
 	);
