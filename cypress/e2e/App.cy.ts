1fd2e66cac15f92f82c5c537b8c5b7b38a062030
gloria <gloriateodoro1@gmail.com>
2024-07-04 13:53:54 -0300

    feat: add cypress test

--- cypress/e2e/App.cy.ts
+++ cypress/e2e/App.cy.ts
@@ -1,6 +1,91 @@
 describe("App", () => {
-	it("should render heading", () => {
+	it("should render all fields", () => {
 		cy.visit("/");
-		cy.findByRole("button", { name: /continuar/i }).should("exist");
+
+		cy.get("#name").type("MSR teste");
+		cy.get("#email").type("msr@test.com");
+		cy.get("#confirmEmail").type("msr@test.com");
+		cy.get("#whatsapp").type("(81) 99999-9999");
+
+		cy.findByRole("button", { name: "Continuar" }).should("exist");
+	});
+
+	it("should display error if name field is empty", () => {
+		cy.visit("/");
+
+		cy.get("#email").type("msr@test.com");
+		cy.get("#confirmEmail").type("test@test.com");
+		cy.get("#whatsapp").type("(81) 99999-9999");
+
+		cy.findByRole("button", { name: "Continuar" }).should("exist").click();
+		cy.contains("Esse campo é obrigatório.").should("exist");
+	});
+
+	it("should display error if email field is empty", () => {
+		cy.visit("/");
+
+		cy.get("#name").type("MSR teste");
+		cy.get("#confirmEmail").type("test@test.com");
+		cy.get("#whatsapp").type("(81) 99999-9999");
+
+		cy.findByRole("button", { name: "Continuar" }).click();
+		cy.contains("Esse campo é obrigatório.").should("exist");
+	});
+
+	it("should display error if confirmEmail field is empty", () => {
+		cy.visit("/");
+		cy.get("#name").type("MSR teste");
+		cy.get("#email").type("test@test.com");
+		cy.get("#whatsapp").type("(81) 99999-9999");
+
+		cy.findByRole("button", { name: "Continuar" }).click();
+		cy.contains("Esse campo é obrigatório.").should("exist");
+	});
+
+	it("should display error if whatsapp field is empty", () => {
+		cy.visit("/");
+
+		cy.get("#name").type("MSR teste");
+		cy.get("#email").type("test@test.com");
+		cy.get("#confirmEmail").type("test@test.com");
+
+		cy.findByRole("button", { name: "Continuar" }).click();
+		cy.contains("Esse campo é obrigatório.").should("exist");
+	});
+
+	it("should display error if confirm email does not match email", () => {
+		cy.visit("/");
+
+		cy.get("#name").type("MSR teste");
+		cy.get("#email").type("msr@test.com");
+		cy.get("#confirmEmail").type("test@test");
+
+		cy.findByRole("button", { name: "Continuar" }).click();
+		cy.contains("Os e-mails precisam ser iguais.").should("exist");
+	});
+
+	it("should display error if invalid whatsapp format is entered", () => {
+		cy.visit("/");
+
+		cy.get("#name").type("MSR teste");
+		cy.get("#email").type("msr@test.com");
+		cy.get("#confirmEmail").type("msr@test.com");
+		cy.get("#whatsapp").type("9999");
+
+		cy.findByRole("button", { name: "Continuar" }).click();
+		cy.contains("Insira um número de telefone válido com DDD.").should("exist");
+	});
+
+	it("should continue to the next step if all fields are filled correctly", () => {
+		cy.visit("/");
+
+		cy.get("#name").type("MSR teste");
+		cy.get("#email").type("msr@test.com");
+		cy.get("#confirmEmail").type("msr@test.com");
+		cy.get("#whatsapp").type("81999999999");
+
+		cy.findByRole("button", { name: "Continuar" }).click();
+
+		cy.get('[for="sim"]').should("exist");
 	});
 });
