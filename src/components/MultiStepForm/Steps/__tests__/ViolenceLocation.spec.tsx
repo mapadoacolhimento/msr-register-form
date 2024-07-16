12450b4b8600f3eb3ac2d325ac7483fa73895377
gloria <gloriateodoro1@gmail.com>
2024-07-16 18:09:45 -0300

    feat: add ViolenceLocation test

---
+++ src/components/MultiStepForm/Steps/__tests__/ViolenceLocation.spec.tsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import ViolenceLocation from "../ViolenceLocation";
+import MultiStepFormWrapper from "../../MultiStepFormWrapper";
+import { sleep } from "../../../../lib";
+import { type Values } from "../..";
+import { violenceLocationOptions } from "../../../../lib/constants";
+
+const setup = () => {
+	return render(
+		<MultiStepFormWrapper
+			onSubmit={async (values) =>
+				await sleep(300).then(() => console.log(values))
+			}
+			initialValues={
+				{
+					violenceLocation: "",
+				} as Values
+			}
+		>
+			{ViolenceLocation()}
+		</MultiStepFormWrapper>
+	);
+};
+
+describe("<ViolenceLocation />", () => {
+	it("should render fields", () => {
+		setup();
+
+		violenceLocationOptions.forEach((option) => {
+			const optionElement = screen.getByText(option.name);
+			expect(optionElement).toBeInTheDocument();
+		});
+	});
+
+	it("should render an error if no option is selected", async () => {
+		setup();
+
+		const btn = screen.getByRole("button", { name: /enviar/i });
+		await userEvent.click(btn);
+
+		await screen.findByRole("alert");
+
+		expect(screen.getByRole("alert")).toHaveTextContent(
+			"Esse campo é obrigatório."
+		);
+	});
+});
