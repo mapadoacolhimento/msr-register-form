c876578ec283f3dddb08279cd93140116c467e82
gloria <gloriateodoro1@gmail.com>
2024-07-25 15:31:40 -0300

    feat: add component to reuse extra support on denied screens

--- src/components/MultiStepForm/Steps/DeniedScreens/CriteriaDenied.tsx
+++ src/components/MultiStepForm/Steps/DeniedScreens/CriteriaDenied.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { Flex, Grid, Heading, Text } from "@radix-ui/themes";
+import { Grid } from "@radix-ui/themes";
 import Header from "../../../Header";
 import HeaderComponent from "./components/Header";
-import InfoBox from "./components/InfoBox";
-import Image from "next/image";
 import BackgroundIllustration from "./components/BackgroundIllustration";
 import Contact from "./components/Contact";
+import ExtraSupport from "./components/ExtraSupport";
 
 export default function CriteriaDenied() {
 	return (
@@ -16,27 +15,7 @@ export default function CriteriaDenied() {
 					title="Sentimos muito"
 					subtitle="O Mapa do Acolhimento atende mulheres cis, trans ou travestis maiores de 18 anos, que vivem no Brasil e enfrentam situações de vulnerabilidade socioeconômica."
 				>
-					<Heading as={"h2"} size={"6"} align={"center"} mt={"3rem"}>
-						Como podemos te ajudar:
-					</Heading>
-					<Flex maxWidth={"30rem"}>
-						<Text align={"center"}>
-							Conheça a rede de apoio que você pode acessar e um material
-							preparado com cuidado para te ajudar nesse momento difícil:
-						</Text>
-					</Flex>
-					<InfoBox
-						imgSrc="/illustrations/woman-getting-support.svg"
-						imgAlt="Mulher recebendo atendimento por outra mulher"
-						title="Onde e como posso pedir ajuda?"
-						description="Conheça os serviços públicos de proteção que você pode acessar"
-					/>
-					<InfoBox
-						imgSrc="/illustrations/woman-covering-ears.svg"
-						imgAlt="Mulher de cabeça baixa tampando os ouvidos"
-						title="Sofri violência, e agora?"
-						description="Um guia prático para deixar o ciclo da violência"
-					/>
+					<ExtraSupport />
 					<Contact />
 				</HeaderComponent>
 			</div>
--- src/components/MultiStepForm/Steps/DeniedScreens/SupportInProgress.tsx
+++ src/components/MultiStepForm/Steps/DeniedScreens/SupportInProgress.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { Flex, Grid, Heading, Text } from "@radix-ui/themes";
+import { Grid } from "@radix-ui/themes";
 import Header from "../../../Header";
 import HeaderComponent from "./components/Header";
-import InfoBox from "./components/InfoBox";
-import Image from "next/image";
 import BackgroundIllustration from "./components/BackgroundIllustration";
 import Contact from "./components/Contact";
+import ExtraSupport from "./components/ExtraSupport";
 
 export default function SupportInProgress() {
 	return (
@@ -16,27 +15,7 @@ export default function SupportInProgress() {
 					title="Você já recebeu uma voluntária"
 					subtitle="Verificamos que você já solicitou ajuda anteriormente. O contato da voluntária foi enviado para o seu e-mail. De toda forma, entraremos em contato com você por e-mail em até 3 dias úteis para compreender o que houve e, se necessário, te indicar outra voluntária. Se desejar, pode nos contatar diretamente pelo e-mail atendimento@mapadoacolhimento.org Obrigada pela confiança!"
 				>
-					<Heading as={"h2"} size={"6"} align={"center"} mt={"3rem"}>
-						Como podemos te ajudar:
-					</Heading>
-					<Flex maxWidth={"30rem"}>
-						<Text align={"center"}>
-							Conheça a rede de apoio que você pode acessar e um material
-							preparado com cuidado para te ajudar nesse momento difícil:
-						</Text>
-					</Flex>
-					<InfoBox
-						imgSrc="/illustrations/woman-getting-support.svg"
-						imgAlt="Mulher recebendo atendimento por outra mulher"
-						title="Onde e como posso pedir ajuda?"
-						description="Conheça os serviços públicos de proteção que você pode acessar"
-					/>
-					<InfoBox
-						imgSrc="/illustrations/woman-covering-ears.svg"
-						imgAlt="Mulher de cabeça baixa tampando os ouvidos"
-						title="Sofri violência, e agora?"
-						description="Um guia prático para deixar o ciclo da violência"
-					/>
+					<ExtraSupport />
 					<Contact />
 				</HeaderComponent>
 			</div>
