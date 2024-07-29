6801d4800454938011c8d196551878b25788db51
Viviane Dias <vivi@mapa.org.br>
2024-07-29 17:05:40 -0300

    feat: creates BaseFormLayout

--- src/components/index.ts
+++ src/components/index.ts
@@ -8,3 +8,4 @@ export { default as RadioInput } from "./RadioInput";
 export { default as CheckboxInput } from "./CheckboxInput";
 export { default as SelectInput } from "./SelectInput";
 export { default as HoverInfo } from "./HoverInfo";
+export { default as BaseFormLayout } from "./BaseFormLayout";
