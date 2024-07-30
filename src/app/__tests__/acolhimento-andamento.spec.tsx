cc2a3329311a3e5ca436bd266384e6e0a8045336
Viviane Dias <vivi@mapa.org.br>
2024-07-29 22:51:42 -0300

    test: adds tests to static pages

---
+++ src/app/__tests__/acolhimento-andamento.spec.tsx
@@ -0,0 +1,91 @@
+import { render, screen } from "@testing-library/react";
+import SupportInProgress from "../acolhimento-andamento/page";
+
+const setup = () => {
+	return render(<SupportInProgress />);
+};
+
+describe("<SupportInProgress />", () => {
+	it("should render headers and descriptions", () => {
+		setup();
+
+		const heading1 = screen.getByRole("heading", {
+			name: "Você já recebeu uma voluntária",
+		});
+		expect(heading1).toBeInTheDocument();
+
+		const heading2 = screen.getByRole("heading", {
+			name: "Como podemos te ajudar:",
+		});
+		expect(heading2).toBeInTheDocument();
+
+		const description = screen.getByText(
+			"Verificamos que você já solicitou ajuda anteriormente. O contato da voluntária foi enviado para o seu e-mail."
+		);
+		expect(description).toBeInTheDocument();
+
+		const link = screen.getByRole("link", {
+			name: "atendimento@mapadoacolhimento.org",
+		});
+		expect(link).toBeInTheDocument();
+
+		const description2 = screen.getByText(
+			"Conheça a rede de apoio que você pode acessar e um material preparado com cuidado para te ajudar nesse momento difícil:"
+		);
+		expect(description2).toBeInTheDocument();
+	});
+
+	it("should render help cards", () => {
+		setup();
+
+		const image1 = screen.getByRole("img", {
+			name: "Mulher recebendo atendimento por outra mulher",
+		});
+		expect(image1).toBeInTheDocument();
+
+		const heading1 = screen.getByRole("heading", {
+			name: "Onde e como posso pedir ajuda?",
+			level: 3,
+		});
+		expect(heading1).toBeInTheDocument();
+
+		const description1 = screen.getByText(
+			"Conheça os serviços públicos de proteção que você pode acessar."
+		);
+		expect(description1).toBeInTheDocument();
+
+		const image2 = screen.getByRole("img", {
+			name: "Mulher de cabeça baixa tampando os ouvidos",
+		});
+		expect(image2).toBeInTheDocument();
+
+		const heading2 = screen.getByRole("heading", {
+			name: "Sofri violência, e agora?",
+			level: 3,
+		});
+		expect(heading2).toBeInTheDocument();
+
+		const description2 = screen.getByText(
+			"Um guia prático para deixar o ciclo da violência."
+		);
+		expect(description2).toBeInTheDocument();
+	});
+
+	it("should contact text", () => {
+		setup();
+
+		const heartIcon = screen.getByRole("img", {
+			name: "Balão de diálogo roxo com um coração dentro",
+		});
+		expect(heartIcon).toBeInTheDocument();
+
+		const text1 = screen.getByText("Ficou com alguma dúvida?");
+		expect(text1).toBeInTheDocument();
+
+		const text2 = screen.getByText("Fale conosco em");
+		expect(text2).toBeInTheDocument();
+
+		const link = screen.getByRole("link", { name: "contato@mapa.org.br" });
+		expect(link).toBeInTheDocument();
+	});
+});
