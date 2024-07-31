a4e8f0824109d96a8b7c27a147ea6ff6ace127d7
gloria <gloriateodoro1@gmail.com>
2024-07-31 11:38:49 -0300

    feat: add Header Component

---
+++ src/components/MultiStepForm/Steps/HomeScreen/HeaderComponent.tsx
@@ -0,0 +1,33 @@
+import { Heading, Box, Flex, Text } from "@radix-ui/themes";
+
+interface HeaderComponentProps {
+	title: string;
+	subtitle?: React.ReactNode;
+	children?: React.ReactNode;
+}
+
+const HeaderComponent: React.FC<HeaderComponentProps> = ({
+	title,
+	subtitle,
+	children,
+}) => (
+	<Box
+		style={{
+			display: "flex",
+			flexDirection: "column",
+			alignItems: "center",
+		}}
+	>
+		<Heading as={"h2"} size={"8"} align={"center"}>
+			{title}
+		</Heading>
+		{subtitle && (
+			<Flex maxWidth={"30rem"}>
+				<Text align={"center"}>{subtitle}</Text>
+			</Flex>
+		)}
+		{children}
+	</Box>
+);
+
+export default HeaderComponent;
