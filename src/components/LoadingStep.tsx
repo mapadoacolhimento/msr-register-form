6b0d85155b9c027054de5c1916031b393bfb871d
gloria <gloriateodoro1@gmail.com>
2024-08-06 16:57:16 -0300

    feat: add loading step

---
+++ src/components/LoadingStep.tsx
@@ -0,0 +1,52 @@
+import { Box, Flex, Heading, Text } from "@radix-ui/themes";
+import DesktopIllustration from "./DesktopIllustration";
+import Image from "next/image";
+
+export default function LoadingStep() {
+	return (
+		<Flex
+			width={"100%"}
+			justify={"center"}
+			py={{ initial: "8", md: "9" }}
+			px={{ initial: "6", md: "7" }}
+		>
+			<Flex direction={"column"} align={"center"} maxWidth={"30rem"}>
+				<Box asChild pb={"4"}>
+					<Heading
+						as={"h1"}
+						size={"8"}
+						align={"center"}
+						color={"purple"}
+						highContrast
+					>
+						Agora é só esperar
+					</Heading>
+				</Box>
+				<Flex
+					align="center"
+					gap="4"
+					direction={"column"}
+					maxWidth={"20rem"}
+					justify={"center"}
+				>
+					<Image
+						src="/icons/spinner.svg"
+						alt="Ícone de círculo em movimento indicando carregamento"
+						width={50}
+						height={50}
+					/>
+					<Text align={"center"}>
+						Nesse momento estamos analisando seus dados e em breve buscaremos
+						uma voluntária para te atender
+					</Text>
+				</Flex>
+			</Flex>
+			<DesktopIllustration
+				img={{
+					src: "/illustrations/woman.svg",
+					alt: "Ilustração de uma mulher com cabelo castanho escuro e blusa roxa com um coração branco do mapa do acolhimento",
+				}}
+			/>
+		</Flex>
+	);
+}
