223f2d3449963382bbeca0bbfdf3e0f2735a44e8
Viviane Dias <vivi@mapa.org.br>
2024-08-08 17:43:37 -0300

    fix: updates normalizeCity func after SonarCloud security warning

--- src/lib/__tests__/normalizeCity.spec.ts
+++ src/lib/__tests__/normalizeCity.spec.ts
@@ -7,9 +7,40 @@ describe("normalizeCity", () => {
 		expect(result).toStrictEqual("not_found");
 	});
 
+	it("should return 'not_found' if city is falsy", () => {
+		// @ts-expect-error
+		expect(normalizeCity(undefined)).toStrictEqual("not_found");
+		// @ts-expect-error
+		expect(normalizeCity(null)).toStrictEqual("not_found");
+	});
+
 	it("should normalize and uppercase the city name", () => {
 		const city = "São Paulo";
 		const result = normalizeCity(city);
 		expect(result).toStrictEqual("SAO PAULO");
+	});
+
+	it("should remove diacritics from the city name", () => {
+		const city = "São Paulo";
+		const result = normalizeCity(city);
+		expect(result).toStrictEqual("SAO PAULO");
+	});
+
+	it("should remove single quotes from the city name", () => {
+		const city = "New 'York'";
+		const result = normalizeCity(city);
+		expect(result).toStrictEqual("NEW YORK");
+	});
+
+	it("should remove double quotes from the city name", () => {
+		const city = 'Los "Angeles"';
+		const result = normalizeCity(city);
+		expect(result).toStrictEqual("LOS ANGELES");
+	});
+
+	it("should remove parentheses and their contents from the city name", () => {
+		const city = "San Francisco (CA)";
+		const result = normalizeCity(city);
+		expect(result).toStrictEqual("SAN FRANCISCO");
 	});
 });
--- src/lib/normalizeCity.ts
+++ src/lib/normalizeCity.ts
@@ -1,10 +1,11 @@
 export default function normalizeCity(city: string): string {
-	if (city === "not_found") return "not_found";
+	if (city === "not_found" || !city) return "not_found";
 
 	return city
 		.normalize("NFD")
 		.replace(/[\u0300-\u036f]/g, "")
-		.replace("'", " ")
-		.replace(/ *\([^)]*\) */g, "")
-		.toUpperCase();
+		.replace(/['"]/g, "")
+		.replace(/\([^()]*\)/g, "")
+		.toUpperCase()
+		.trim();
 }
